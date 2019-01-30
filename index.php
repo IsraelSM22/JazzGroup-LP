a2ba6182fa2241bc4859f5581ca6474dd199f4df
Jaime Israel Sánchez Martínez <37377933+IsraelSM22@users.noreply.github.com>
2019-01-30 17:46:46 -0600

    Primer avance

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Jazz Group | LP</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- SCSS -->
+    <link rel="stylesheet" href="assets/scss/styles.css">
+
+    <!-- JQuery -->
+    <script src="assets/libs/jquery-3.3.1.min.js"></script>
+
+    <!-- Bootstrap 4.1.3 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+
+    <!-- Font Awesome v5.6.3 -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+        crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid main-container">
+        <div class="row justify-content-between text-center container-header">
+            <div class="col-md-4 container-logo">LOGO</div>
+            <div class="col-md-7 main-container-menu">
+                <div class="row container-menu">
+                    <div class="col-md-4 menu-item">
+                        HOME
+                    </div>
+                    <div class="col-md-4 menu-item">
+                        PARTICIPACIÓN
+                    </div>
+                    <div class="col-md-4 menu-item">
+                        CONDICIONES
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row text-center banner-section">
+            <div class="col-md-12 title-banner">HEADLINE</div>
+            <div class="col-md-12 text-banner">Lorem ipsum dolor sit amet consectetur adipiscing elit, pulvinar mi habitant
+                torquent dui cras luctus platea,
+                iaculis bibendum curae fringilla dictum eget. Feugiat tellus facilisi rutrum porta felis purus
+                praesent, metus
+                inceptos in ullamcorper cum lacus ligula, interdum nascetur turpis donec ac habitant.</div>
+        </div>
+        <div class="row mid-section">
+            <div class="col-md-6 text-center align-items-center c-video">VIDEO FORMATO VERTICAL (4:5, 1080x1350)</div>
+            <div class="col-md-6 c-points-mid-section">
+                <div class="row container-points">
+                    <div class="col-md-12 text-center title-points">TITLE</div>
+                    <div class="col-md-12 text-center description-points">Lorem ipsum dolor sit amet consectetur adipiscing elit,
+                        pulvinar mi habitant torquent dui cras luctus platea,</div>
+                    <div class="col-md-12 cont-tree-points">
+                        <div class="row item-points">
+                            <div class="col-md-1 text-center number">1</div>
+                            <div class="col-md-11 text-left description">Lorem ipsum dolor sit amet consectetur adipiscing
+                                elit, pulvinar mi habitant torquent dui cras luctus
+                                platea 
+                            </div>
+                        </div>
+                        <div class="row item-points">
+                            <div class="col-md-1 text-center number">2</div>
+                            <div class="col-md-11 text-left description">Lorem ipsum dolor sit amet consectetur adipiscing
+                                elit, pulvinar mi habitant torquent dui cras luctus
+                                platea
+                            </div>
+                        </div>
+                        <div class="row item-points">
+                            <div class="col-md-1 text-center number">3</div>
+                            <div class="col-md-11 text-left description">Lorem ipsum dolor sit amet consectetur adipiscing
+                                elit, pulvinar mi habitant torquent dui cras luctus
+                                platea
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 cont-share">SOCIAL MEDIA SHARE BUTTON</div>
+                </div>
+            </div>
+        </div>
+        <div class="row text-center bottom-section">
+            <div class="col-md-12 bottom-title">TITLE</div>
+            <div class="col-md-12 bottom-text">Lorem ipsum dolor sit amet consectetur adipiscing elit, pulvinar mi habitant
+                torquent dui cras luctus platea,
+                iaculis bibendum curae fringilla dictum eget. Feugiat tellus facilisi rutrum porta felis purus
+                praesent, metus
+                inceptos in ullamcorper cum lacus ligula, interdum nascetur turpis donec ac habitant.</div>
+            <div class="col-md-12 bottom-text">Lorem ipsum dolor sit amet consectetur adipiscing elit, pulvinar mi habitant
+                torquent dui cras luctus platea,
+                iaculis bibendum curae fringilla dictum eget. Feugiat tellus facilisi rutrum porta felis purus
+                praesent, metus
+                inceptos in ullamcorper cum lacus ligula, interdum nascetur turpis donec ac habitant.</div>
+            <div class="col-md-12 bottom-text">Lorem ipsum dolor sit amet consectetur adipiscing elit, pulvinar mi habitant
+                torquent
+                dui cras luctus platea,
+                iaculis bibendum curae fringilla dictum eget. Feugiat tellus facilisi rutrum porta felis purus
+                praesent,
+                metus
+                inceptos in ullamcorper cum lacus ligula, interdum nascetur turpis donec ac habitant.</div>
+        </div>
+        <div class="col-md-12 text-center bottom-legal">LEGALES</div>
+
+    </div>
+
+    </div>
+</body>
+
+</html>
