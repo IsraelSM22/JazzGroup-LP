f9665a6842be2b240868cb02c5fdf6582fcd1140
Jaime Israel Sánchez Martínez <37377933+IsraelSM22@users.noreply.github.com>
2019-01-31 17:43:29 -0600

    Avances

--- index.php
+++ index.php
@@ -25,87 +25,149 @@
 </head>
 
 <body>
-    <div class="container-fluid main-container">
-        <div class="row justify-content-between text-center container-header">
-            <div class="col-md-4 container-logo">LOGO</div>
-            <div class="col-md-7 main-container-menu">
-                <div class="row container-menu">
-                    <div class="col-md-4 menu-item">
-                        HOME
-                    </div>
-                    <div class="col-md-4 menu-item">
-                        PARTICIPACIÓN
-                    </div>
-                    <div class="col-md-4 menu-item">
-                        CONDICIONES
-                    </div>
-                </div>
-            </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="Jazz Group">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse w-100" id="navbarSupportedContent">
+            <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="participaciones.php">Participaciones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Términos y Condiciones</a>
+                </li>
+            </ul>
         </div>
-        <div class="row text-center banner-section">
-            <div class="col-md-12 title-banner">HEADLINE</div>
-            <div class="col-md-12 text-banner">Lorem ipsum dolor sit amet consectetur adipiscing elit, pulvinar mi habitant
-                torquent dui cras luctus platea,
-                iaculis bibendum curae fringilla dictum eget. Feugiat tellus facilisi rutrum porta felis purus
-                praesent, metus
-                inceptos in ullamcorper cum lacus ligula, interdum nascetur turpis donec ac habitant.</div>
-        </div>
-        <div class="row mid-section">
-            <div class="col-md-6 text-center align-items-center c-video">VIDEO FORMATO VERTICAL (4:5, 1080x1350)</div>
-            <div class="col-md-6 c-points-mid-section">
-                <div class="row container-points">
-                    <div class="col-md-12 text-center title-points">TITLE</div>
-                    <div class="col-md-12 text-center description-points">Lorem ipsum dolor sit amet consectetur adipiscing elit,
-                        pulvinar mi habitant torquent dui cras luctus platea,</div>
-                    <div class="col-md-12 cont-tree-points">
-                        <div class="row item-points">
-                            <div class="col-md-1 text-center number">1</div>
-                            <div class="col-md-11 text-left description">Lorem ipsum dolor sit amet consectetur adipiscing
-                                elit, pulvinar mi habitant torquent dui cras luctus
-                                platea 
-                            </div>
-                        </div>
-                        <div class="row item-points">
-                            <div class="col-md-1 text-center number">2</div>
-                            <div class="col-md-11 text-left description">Lorem ipsum dolor sit amet consectetur adipiscing
-                                elit, pulvinar mi habitant torquent dui cras luctus
-                                platea
-                            </div>
-                        </div>
-                        <div class="row item-points">
-                            <div class="col-md-1 text-center number">3</div>
-                            <div class="col-md-11 text-left description">Lorem ipsum dolor sit amet consectetur adipiscing
-                                elit, pulvinar mi habitant torquent dui cras luctus
-                                platea
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-12 cont-share">SOCIAL MEDIA SHARE BUTTON</div>
-                </div>
-            </div>
-        </div>
-        <div class="row text-center bottom-section">
-            <div class="col-md-12 bottom-title">TITLE</div>
-            <div class="col-md-12 bottom-text">Lorem ipsum dolor sit amet consectetur adipiscing elit, pulvinar mi habitant
-                torquent dui cras luctus platea,
-                iaculis bibendum curae fringilla dictum eget. Feugiat tellus facilisi rutrum porta felis purus
-                praesent, metus
-                inceptos in ullamcorper cum lacus ligula, interdum nascetur turpis donec ac habitant.</div>
-            <div class="col-md-12 bottom-text">Lorem ipsum dolor sit amet consectetur adipiscing elit, pulvinar mi habitant
-                torquent dui cras luctus platea,
-                iaculis bibendum curae fringilla dictum eget. Feugiat tellus facilisi rutrum porta felis purus
-                praesent, metus
-                inceptos in ullamcorper cum lacus ligula, interdum nascetur turpis donec ac habitant.</div>
-            <div class="col-md-12 bottom-text">Lorem ipsum dolor sit amet consectetur adipiscing elit, pulvinar mi habitant
-                torquent
-                dui cras luctus platea,
-                iaculis bibendum curae fringilla dictum eget. Feugiat tellus facilisi rutrum porta felis purus
-                praesent,
-                metus
-                inceptos in ullamcorper cum lacus ligula, interdum nascetur turpis donec ac habitant.</div>
-        </div>
-        <div class="col-md-12 text-center bottom-legal">LEGALES</div>
+    </nav>
 
+    <div class="content-delimiter" style="width: 100%; max-width:1200px; margin: 0 auto;">
+        <div class="container-fluid main-container">
+
+            <div class="row text-center banner-section">
+                <div class="col-md-12 title-banner exe_Bold">AIR FRANCE GOURMET</div>
+                <div class="col-md-12 subtitle-banner exe_Med">En febrero México y Francia están más cerca de lo que
+                    imaginas.</div>
+                <div class="col-md-12 text-banner open-reg">Vive un Día de la Candelaria a la francesa con crêpes…
+                    Participa y podrás disfrutar de esta gran experiencia con un acompañante en París, una de las
+                    ciudades
+                    más reconocidas del mundo por su arte, cultura y gastronomía.</div>
+            </div>
+            <div class="row mid-section">
+                <div class="col-md-6 text-center align-items-center">
+                    <div class="c-video">
+                        VIDEO FORMATO VERTICAL (4:5, 1080x1350)
+                    </div>
+                </div>
+                <div class="col-md-6 c-points-mid-section">
+                    <div class="row container-points open-reg">
+                        <div class="col-md-12">
+                            <div class="button-share-mobile">SOCIAL MEDIA SHARE BUTTON</div>
+                        </div>
+                        <div class="col-md-12 text-center title-points">¿CÓMO PARTICIPAR?</div>
+                        <div class="col-md-12 cont-tree-points">
+                            <div class="row item-points">
+                                <div class="col-md-1 text-center number">1</div>
+                                <div class="col-md-11 text-left description">Prepara tu propia receta de Crêpes y ponle
+                                    un
+                                    toque creativo al uso
+                                    de ingredientes y presentación.
+                                </div>
+                            </div>
+                            <div class="row item-points">
+                                <div class="col-md-1 text-center number">2</div>
+                                <div class="col-md-11 text-left description">Toma una fotografía o video de tu Crêpe.</div>
+                            </div>
+                            <div class="row item-points">
+                                <div class="col-md-1 text-center number">3</div>
+                                <div class="col-md-11 text-left description">Súbela a tu Instagram describiendo lo que
+                                    usaste en tu receta y usa el hashtag #AirFranceGourmet.
+                                    Recuerda que tu publicación deberá estar como publica para poder participar.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12 text-center description-points">Un jurado elegirá las cinco recetas más
+                            creativas que participarán en la gran final, los seleccionados tendrán que preparar su
+                            Crêpe en
+                            vivo ante nuestros
+                            jueces para seleccionar al ganador que se llevará 2 boletos redondos a Paris, Francia.</div>
+                        <div class="col-md-12">
+                            <div class="button-share">SOCIAL MEDIA SHARE BUTTON</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row bottom-section">
+                <div class="col-md-12 frase text-center">¡Tienes hasta el 17 de febrero de 2019 para participar!</div>
+                <div class="col-md-12 bottom-title exe_Bold text-center">¿CÓMO PREPARAR UNA CRÊPE?</div>
+                <div class="col-md-12 bottom-list">
+                    <span>Receta Crêpes(para 4 personas):</span>
+                    <ul class="">
+                        <li class="">250g de harina</li>
+                        <li class="">4 huevos</li>
+                        <li class="">½ L de leche </li>
+                        <li class="">1 pizca de sal</li>
+                        <li class="">50 gr de mantequilla derretida</li>
+                    </ul>
+                </div>
+                <div class="col-md-12 text-center">
+                    <span>Si lo deseas puedes agregar aroma a base de vainilla, azahar (flor del naranjo) o algún
+                        ingrediente
+                        sorpresa que consideres ¡Se creativo!</span>
+                </div>
+                <div class="col-md-12 bottom-list">
+                    <span>La Crêpe la puedes rellenar de lo que gustes, puede ser:</span>
+
+                    <ul class="">
+                        <li class="">Fruta fresca: fresas, moras, frambuesas, banano, mango, etc.</li>
+                        <li class="">Dulce de leche</li>
+                        <li class="">Crema de chocolate y avellanas</li>
+                        <li class="">Salsa casera de fresas, mermeladas o compotas de frutas a tu gusto</li>
+                        <li class="">Azúcar / Miel de abeja</li>
+                        <li class="">Helado de vainilla</li>
+                    </ul>
+                </div>
+                <div class="col-md-12 text-center">
+                    <span>También las puedes preparar saladas si gustas.</span>
+                </div>
+                <div class="col-md-9 bottom-list">
+                    <span>Preparación:</span>
+
+                    <ol class="">
+                        <li class="">Coloca en un tazón una taza y 1/2 de harina de trigo y una pizca de sal. Añade el
+                            huevo (clara y yema), bate con un batidor de globo.</li>
+                        <li class="">A la mezcla viértele la mitad de la taza de leche poco a poco y mezcla nuevamente
+                            con
+                            el batidor, hasta que se incorporen los ingredientes.</li>
+                        <li class="">En el proceso de incorporación de los ingredientes añade el resto de la leche y
+                            bate
+                            hasta deshacer los grumos.</li>
+                        <li class="">Agrega la mantequilla derretida e intégrala hasta dejar una mezcla homogénea.</li>
+                        <li class="">Vierte una cucharada de mantequilla derretida o aceite en un sartén de teflón y
+                            espárcela por todo el espacio.</li>
+                        <li class="">Toma un poco de la preparación con un cucharón y vierte sobre el sartén poco a
+                            poco.
+                            Gira la sartén para esparcir la mezcla en toda la sartén.</li>
+                        <li class="">Cocina la mezcla a fuego medio por 2 minutos o hasta que la crepa se despegue de
+                            las
+                            orillas.</li>
+                        <li class="">Voltea la crepa y cocina por 2 minutos más o hasta que se dore ligeramente.</li>
+                    </ol>
+                </div>
+                <div class="col-md-12 text-center">
+                    <span>¡Listo, es momento de rellenar tu crêpe con lo que más te guste!</span>
+
+                </div>
+            </div>
+            <div class="col-md-12 text-center bottom-legal">LEGALES</div>
+        </div>
     </div>
 
     </div>
---
+++ participaciones.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Jazz Group | LP</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- SCSS -->
+    <link rel="stylesheet" href="assets/scss/styles.css">
+
+    <!-- JQuery -->
+    <script src="assets/libs/jquery-3.3.1.min.js"></script>
+
+    <!-- Bootstrap 4.1.3 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+
+    <!-- Font Awesome v5.6.3 -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+        crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="Jazz Group">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse w-100" id="navbarSupportedContent">
+            <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Inicio</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="participaciones.php">Participaciones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Términos y Condiciones</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="content-delimiter fs60" style="width: 100%; max-width:1200px; margin: 0 auto;">
+        <div class="container-fluid ">
+            <div class="row">
+                <div class="col-md-12 text-center exe_Bold">Participaciones</div>
+                <div class="col-md-12">
+                    <!-- Stackla Widget Embed Code (start) -->
+                    <div class="stackla-widget" data-alias="airfrance.stackla.com" data-ct="" data-hash="5c473eb0ecf15"
+                        data-id="51983" data-title="Social Wall Mexico" data-ttl="30" style="width: 100%; overflow: hidden;"></div>
+                    <!-- Stackla Widget Embed Code (end) -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+    <script type="text/javascript">
+        (function (d, id) {
+            var t, el = d.scripts[d.scripts.length - 1].previousElementSibling;
+            if (el) el.dataset.initTimestamp = (new Date()).getTime();
+            if (d.getElementById(id)) return;
+            t = d.createElement('script');
+            t.src = '//assetscdn.stackla.com/media/js/widget/fluid-embed.js';
+            t.id = id;
+            (d.getElementsByTagName('head')[0] || d.getElementsByTagName('body')[0]).appendChild(t);
+        }(document, 'stackla-widget-js'));
+    </script>
+
+</body>
+
+</html>
