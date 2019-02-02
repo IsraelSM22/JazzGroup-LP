39cc12f749b1ec0b6bdbfa30b08bf23fb58a5aa3
Oscar Sánchez <oscar.sanjim@gmail.com>
2019-02-01 18:19:16 -0600

    Avances

---
+++ layouts/navigation.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oscarsanchez
+ * Date: 2/1/19
+ * Time: 14:07
+ */
+
+$BASE_URL = "/airfrance/"
+
+?>
+
+<nav class="navbar navbar-expand-lg navbar-light ">
+    <a class="navbar-brand" href="#">
+        <img src="<?php echo $BASE_URL ?>assets/images/logo-airfrance.svg" width="250"
+             alt="Jazz Group">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse w-100" id="navbarSupportedContent">
+        <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo $BASE_URL;?>">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $BASE_URL;?>participaciones">Participaciones</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $BASE_URL;?>condiciones">Condiciones</a>
+            </li>
+        </ul>
+    </div>
+</nav>
