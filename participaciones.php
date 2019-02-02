5969cc73548d65acb8fd4fa83e60735f83631178
Oscar Sánchez <oscar.sanjim@gmail.com>
2019-02-01 18:22:41 -0600

    Avances

--- participaciones.php
+++ participaciones.php
@@ -2,30 +2,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <title>Jazz Group | LP</title>
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include __DIR__ . "/layouts/head.php"; ?>
+    <link rel="stylesheet" href="assets/scss/participations.css">
 
-    <!-- SCSS -->
-    <link rel="stylesheet" href="assets/scss/styles.css">
-
-    <!-- JQuery -->
-    <script src="assets/libs/jquery-3.3.1.min.js"></script>
-
-    <!-- Bootstrap 4.1.3 -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-          crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-            crossorigin="anonymous"></script>
-
-    <!-- Font Awesome v5.6.3 -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
-          crossorigin="anonymous">
 </head>
 
 <body>
@@ -37,7 +16,7 @@
 <div class="content-delimiter fs60" style="width: 100%; max-width:1200px; margin: 0 auto;">
     <div class="container-fluid ">
         <div class="row">
-            <div class="col-md-12 text-center exe_Bold">Participaciones</div>
+            <div class="col-md-12 title-banner">PARTICIPACIONES</div>
             <div class="col-md-12">
                 <!-- Stackla Widget Embed Code (start) -->
                 <div class="stackla-widget" data-alias="airfrance.stackla.com" data-ct="" data-hash="5c473eb0ecf15"
