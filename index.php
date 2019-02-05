fc1ec10ec5ac07ac54a9ed534bc8dc20b8b6d615
Jaime Israel Sánchez Martínez <37377933+IsraelSM22@users.noreply.github.com>
2019-02-05 17:16:07 -0600

    Cambios

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $BASE_URL = "/";
 
 
     <style>
-        .home > a{
+        .home>a {
             color: black !important;
 
         }
@@ -21,222 +21,213 @@ $BASE_URL = "/";
 
 <body>
 
-<div id="fb-root"></div>
+    <div id="fb-root"></div>
 
 
-<!-- Including the navigation layout -->
-<?php include __DIR__ . "/layouts/navigation.php"; ?>
+    <!-- Including the navigation layout -->
+    <?php include __DIR__ . "/layouts/navigation.php"; ?>
 
 
-<div class="container-fluid main-container">
+    <div class="container-fluid main-container">
 
-    <div class="content-delimiter">
-        <!-- Title, Subtitle and main text container -->
-        <div class="row text-center banner-section">
+        <div class="content-delimiter">
+            <!-- Title, Subtitle and main text container -->
+            <div class="row text-center banner-section">
 
-            <div class="col-md-12 title main-title">
-                AIR FRANCE GOURMET
-            </div>
+                <div class="col-md-12 title main-title">
+                    AIR FRANCE GOURMET
+                </div>
 
-            <div class="col-md-12 subtitle main-subtitle">
-                En febrero México y Francia están más cerca de lo que
-                imaginas.
-            </div>
+                <div class="col-md-12 subtitle main-subtitle">
+                    En febrero México y Francia están más cerca de lo que imaginas.
+                </div>
 
-            <div class="col-md-12 content-text main-description">
-                Vive un Día de la Candelaria a la francesa con crêpes…
-                Participa y podrás disfrutar de esta gran experiencia con un acompañante en París, una de las
-                ciudades
-                más reconocidas del mundo por su arte, cultura y gastronomía.
+                <div class="col-md-12 content-text main-description">
+                    Vive un Día de la Candelaria a la francesa con crêpes…
+                    Participa y podrás disfrutar de esta gran experiencia con un acompañante en París, una de las
+                    ciudades más reconocidas del mundo por su arte, cultura y gastronomía.
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
-<div class="container-fluid">
-    <!-- Video and Instructions Container -->
-    <div class="content-delimiter">
-        <div class="row mid-section">
+    <div class="container-fluid">
+        <!-- Video and Instructions Container -->
+        <div class="content-delimiter">
+            <div class="row mid-section">
 
-            <div class="col-lg-7 text-center">
-                <div class="video-container">
-                    <iframe src="https://www.youtube.com/embed/yjrK1RxNd-E" frameborder="0"
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                <div class="col-lg-7 text-center">
+                    <div class="video-container">
+                        <iframe src="https://www.youtube.com/embed/yjrK1RxNd-E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen></iframe>
+                    </div>
                 </div>
-            </div>
 
-            <div class="col-lg-5 c-points-mid-section">
-                <div class="row container-points open-reg">
+                <div class="col-lg-5 c-points-mid-section">
+                    <div class="row container-points open-reg">
 
-                    <div class="col-md-12 section-title">
-                        ¿CÓMO PARTICIPAR?
-                    </div>
+                        <div class="col-md-12 section-title">
+                            ¿CÓMO PARTICIPAR?
+                        </div>
 
-                    <div class="col-md-12 content-text instructions">
+                        <div class="col-md-12 content-text instructions">
 
-                        <ol>
-                            <li>Prepara tu propia receta de Crêpes y ponle un toque
-                                creativo al uso de ingredientes y
-                                presentación.
-                            </li>
+                            <ol>
+                                <li>Prepara tu propia receta de Crêpes y ponle un toque
+                                    creativo al uso de ingredientes y presentación.
+                                </li>
 
-                            <li>
-                                Toma una fotografía o video de tu Crêpe.
-                            </li>
+                                <li>
+                                    Toma una fotografía o video de tu Crêpe.
+                                </li>
 
-                            <li>
-                                Súbela a tu Instagram describiendo lo que
-                                usaste en tu receta y usa el hashtag #AirFranceGourmet.
-                                Recuerda que tu publicación deberá estar como publica para poder participar.
-                            </li>
+                                <li>
+                                    Súbela a tu Instagram describiendo lo que usaste en tu receta y usa el hashtag #AirFranceGourmet.
+                                    Recuerda que tu publicación deberá estar como publica para poder participar.
+                                </li>
 
-                            <li>
-                                Un jurado elegirá las tres recetas más creativas que participarán en la gran final, los
-                                seleccionados tendrán que preparar su Crêpe en vivo ante nuestros jueces para
-                                seleccionar al
-                                ganador que se llevará 2 boletos redondos a París, Francia.
-                            </li>
-                        </ol>
-
-                    </div>
-
-                    <div class="col-md-12 hidden">
-                        <div class="button-share content-text">
+                                <li>
+                                    Un jurado elegirá las tres recetas más creativas que participarán en la gran final,
+                                    los seleccionados tendrán que preparar su Crêpe en vivo ante nuestros jueces para seleccionar al
+                                    ganador que se llevará 2 boletos redondos a París, Francia.
+                                </li>
+                            </ol>
 
                         </div>
+
+                        <div class="col-md-12 hidden">
+                            <div class="button-share content-text">
+
+                            </div>
+                        </div>
+
+
                     </div>
+                </div>
+
+                <div class="col-md-12 content-text content-subtitle due-date">
+                    ¡Tienes hasta el 17 de febrero de 2019 para participar!
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <hr class="hr-delimiter">
+
+    <div class="container-fluid">
+        <!-- Recipe Container -->
+        <div class="content-delimiter">
+            <div class="row bottom-section">
+
+
+                <div class="col-md-12 subtitle prepare-coffee">
+                    ¿CÓMO PREPARAR UNA CRÊPE?
+                </div>
+
+                <div class="col-md-12 bottom-list content-text">
+                    <div class="recipe-section">Receta Crêpes (para 4 personas):</div>
+
+
+                    <ul class="lists">
+                        <li class="content-text">250g de harina</li>
+                        <li class="content-text">4 huevos</li>
+                        <li class="content-text">½ L de leche</li>
+                        <li class="content-text">1 pizca de sal</li>
+                        <li class="content-text">50 gr de mantequilla derretida</li>
+                    </ul>
 
 
                 </div>
-            </div>
 
-            <div class="col-md-12 content-text content-subtitle due-date">
-                ¡Tienes hasta el 17 de febrero de 2019 para participar!
-            </div>
-
-        </div>
-    </div>
-</div>
-
-
-<hr class="hr-delimiter">
-
-<div class="container-fluid">
-    <!-- Recipe Container -->
-    <div class="content-delimiter">
-        <div class="row bottom-section">
-
-
-            <div class="col-md-12 subtitle prepare-coffee">
-                ¿CÓMO PREPARAR UNA CRÊPE?
-            </div>
-
-            <div class="col-md-12 bottom-list content-text">
-                <div class="recipe-section">Receta Crêpes (para 4 personas):</div>
-                <ul class="lists">
-                    <li class="content-text">250g de harina</li>
-                    <li class="content-text">4 huevos</li>
-                    <li class="content-text">½ L de leche</li>
-                    <li class="content-text">1 pizca de sal</li>
-                    <li class="content-text">50 gr de mantequilla derretida</li>
-                </ul>
-            </div>
-
-            <div class="col-md-12">
+                <div class="col-md-12">
                     <span class="content-text">
                         Si lo deseas puedes agregar aroma a base de vainilla, azahar (flor del naranjo) o algún
-                        ingrediente
-                        sorpresa que consideres ¡Sé creativo!
+                        ingrediente sorpresa que consideres ¡Sé creativo!
                     </span>
-            </div>
-
-
-            <div class="col-md-12 bottom-list">
-                <div class="recipe-section second-section">
-                    Rellena la crêpe con lo que gustes, puede ser:
                 </div>
 
-                <ul class="lists">
-                    <li class="content-text">Fruta fresca: fresas, moras, frambuesas, banano, mango, etc.</li>
-                    <li class="content-text">Dulce de leche</li>
-                    <li class="content-text">Crema de chocolate y avellanas</li>
-                    <li class="content-text">Salsa casera de fresas, mermeladas o compotas de frutas a tu gusto</li>
-                    <li class="content-text">Azúcar / Miel de abeja</li>
-                    <li class="content-text">Helado de vainilla</li>
-                </ul>
-            </div>
+
+                <div class="col-md-12 bottom-list">
+                    <div class="recipe-section second-section">
+                        Rellena la crêpe con lo que gustes, puede ser:
+                    </div>
+
+                    <ul class="lists">
+                        <li class="content-text">Fruta fresca: fresas, moras, frambuesas, banano, mango, etc.</li>
+                        <li class="content-text">Dulce de leche</li>
+                        <li class="content-text">Crema de chocolate y avellanas</li>
+                        <li class="content-text">Salsa casera de fresas, mermeladas o compotas de frutas a tu gusto</li>
+                        <li class="content-text">Azúcar / Miel de abeja</li>
+                        <li class="content-text">Helado de vainilla</li>
+                    </ul>
+                </div>
 
 
-            <div class="col-md-12">
-                <span class="content-text">También las puedes preparar saladas si gustas.</span>
-            </div>
+                <div class="col-md-12">
+                    <span class="content-text">También las puedes preparar saladas si gustas.</span>
+                </div>
 
 
-            <div class="col-md-9 bottom-list">
-                <span class="recipe-section third-section">Preparación:</span>
+                <div class="col-md-9 bottom-list">
+                    <span class="recipe-section third-section">Preparación:</span>
 
-                <ol class="lists">
-                    <li class="content-text">Coloca en un tazón 1 taza y 1/2 de harina de trigo y una pizca de sal.
-                        Añade el
-                        huevo (clara y yema), bate con un batidor de globo.
-                    </li>
-                    <li class="content-text">A la mezcla viértele la mitad de la taza de leche poco a poco y mezcla
-                        nuevamente
-                        con
-                        el batidor, hasta que se incorporen los ingredientes.
-                    </li>
-                    <li class="content-text">En el proceso de incorporación de los ingredientes añade el resto de la
-                        leche y
-                        bate
-                        hasta deshacer los grumos.
-                    </li>
-                    <li class="content-text">Agrega la mantequilla derretida e intégrala hasta dejar una mezcla
-                        homogénea.
-                    </li>
-                    <li class="content-text">Vierte una cucharada de mantequilla derretida o aceite en un sartén de
-                        teflón y
-                        espárcela por todo el espacio.
-                    </li>
-                    <li class="content-text">Toma un poco de la preparación con un cucharón y vierte sobre la sartén
-                        poco a
-                        poco.
-                        Gira la sartén para esparcir la mezcla en toda la sartén.
-                    </li>
-                    <li class="content-text">Cocina la mezcla a fuego medio por 2 minutos o hasta que la crepa se
-                        despegue de
-                        las
-                        orillas.
-                    </li>
-                    <li class="content-text">Voltea la crepa y cocina por 2 minutos más o hasta que se dore
-                        ligeramente.
-                    </li>
-                </ol>
-            </div>
-            <div class="col-md-12 content-text">
-                <span>¡Listo, es momento de rellenar tu crêpe con lo que más te guste!</span>
 
+                    <ol class="lists">
+                        <li class="content-text">Coloca en un tazón 1 taza y 1/2 de harina de trigo y una pizca de sal.
+                            Añade el huevo (clara y yema), bate con un batidor de globo.
+                        </li>
+                        <li class="content-text">A la mezcla viértele la mitad de la taza de leche poco a poco y mezcla
+                            nuevamente con el batidor, hasta que se incorporen los ingredientes.
+                        </li>
+                        <li class="content-text">En el proceso de incorporación de los ingredientes añade el resto de
+                            la leche y bate hasta deshacer los grumos.
+                        </li>
+                        <li class="content-text">Agrega la mantequilla derretida e intégrala hasta dejar una mezcla
+                            homogénea.
+                        </li>
+                        <li class="content-text">Vierte una cucharada de mantequilla derretida o aceite en un sartén de
+                            teflón y espárcela por todo el espacio.
+                        </li>
+                        <li class="content-text">Toma un poco de la preparación con un cucharón y vierte sobre la
+                            sartén poco a poco. Gira la sartén para esparcir la mezcla en toda la sartén.
+                        </li>
+                        <li class="content-text">Cocina la mezcla a fuego medio por 2 minutos o hasta que la crepa se
+                            despegue de las orillas.
+                        </li>
+                        <li class="content-text">Voltea la crepa y cocina por 2 minutos más o hasta que se dore
+                            ligeramente.
+                        </li>
+                    </ol>
+                </div>
+
+
+
+                <div class="col-md-12 content-text">
+                    <span>¡Listo, es momento de rellenar tu crêpe con lo que más te guste!</span>
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
-<hr class="hr-delimiter">
+    <hr class="hr-delimiter">
 
-<div class="container-fluid">
-    <!-- Legal info container -->
-    <div class="content-delimiter">
-        <div class="col-md-12 content-text bottom-legal">
-            <a href="<?php echo $BASE_URL;?>condiciones">
-                LEGALES
-            </a>
+    <div class="container-fluid">
+        <!-- Legal info container -->
+        <div class="content-delimiter">
+            <div class="col-md-12 content-text bottom-legal">
+                <a href="<?php echo $BASE_URL;?>condiciones">
+                    LEGALES
+                </a>
+            </div>
         </div>
     </div>
-</div>
 
-</div>
+    </div>
 
 
 </body>
